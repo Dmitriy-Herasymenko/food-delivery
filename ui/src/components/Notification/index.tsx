af19d075c75ad75d234e51839321c25e40d040fb
herasymenko-d <dmherasymenko@gmail.com>
2024-03-06 21:25:43 +0200

    add notification

---
+++ ui/src/components/Notification/index.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+
+interface NotificationProps {
+  username: string;
+  message: string;
+  onClose: () => void;
+}
+
+export const Notification: React.FC<NotificationProps> = ({
+  username,
+  message,
+  onClose,
+}) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowTooltip(false);
+      onClose();
+    }, 3000); 
+    return () => clearTimeout(timer);
+  }, [onClose]);
+
+  return (
+    <div
+      className="fixed bottom-4 right-4 flex items-center w-max p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+      role="alert"
+    >
+      <div className="relative">
+        <img
+          className="w-10 h-10 rounded"
+          src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt="Medium avatar"
+          onMouseEnter={() => setShowTooltip(true)}
+          onMouseLeave={() => setShowTooltip(false)}
+        />
+        {showTooltip && (
+          <div className="absolute -top-14 right-1/2 transform translate-x-1/2 bg-gray-900 text-white text-sm p-2 rounded-lg pointer-events-none">
+            {username}
+          </div>
+        )}
+      </div>
+
+      <div className="ms-3 text-sm font-normal">{message}</div>
+      <button
+        type="button"
+        onClick={onClose}
+        className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+        data-dismiss-target="#toast-default"
+        aria-label="Close"
+      >
+        <span className="sr-only">x</span>
+        <svg
+          className="w-3 h-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 14"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
